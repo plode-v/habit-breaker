9b0beb9744ab61d3252661975a0cbdf9238782d4
Plode <84858705+plode-v@users.noreply.github.com>
2023-10-03 12:40:44 +0700

    update products section"

--- src/components/landing-page/ProductSection.tsx
+++ src/components/landing-page/ProductSection.tsx
@@ -22,8 +22,23 @@ const ProductSection = () => {
                 </div>
             </div>
         </div>
-        <div className='flex-1'>
-            Bottom
+        <div className='flex-1 flex'>
+            <div className='flex-1 h-full justify-center items-center flex'>
+                <div className='px-10'>
+                    <h1>lorem ipsum</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <br />
+                    <ul>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    </ul>
+                    <Button variant="heroBanner">Try Now</Button>
+                </div>
+            </div>
+            <div className='flex-1 h-full border'>
+                img
+            </div>
         </div>
     </div>
   )
