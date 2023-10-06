977e6cc0bfb40a6c3e20d3a5d9b8c5106329b3f2
Plode <84858705+plode-v@users.noreply.github.com>
2023-10-06 15:52:48 +0700

    update section sizing

--- src/components/landing-page/Features.tsx
+++ src/components/landing-page/Features.tsx
@@ -3,7 +3,7 @@ import FeatureComponent from './FeatureComponent'
 
 const Features = () => {
   return (
-    <div className='3xl:h-[50vh] h-[80vh] border flex flex-col items-center pb-10 pt-5 bg-neutral-50'>
+    <div className='h-[700px] border flex flex-col items-center pb-10 pt-5 bg-neutral-50'>
         <div className='h-1/2 w-full flex flex-col justify-center items-center'>
             <h1 className='text-[26px] font-[700] capitalize pb-2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h1>
             <p className='text-[14px] text-neutral-500 w-1/2 text-center'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
--- src/components/landing-page/HeroBanner.tsx
+++ src/components/landing-page/HeroBanner.tsx
@@ -6,8 +6,8 @@ import meditationAnimation from "../../public/meditationAnimation.json"
 
 const HeroBanner = () => {
   return (
-    <div className='xl:h-[calc(50vh+60px)] h-[calc(70vh+60px)] w-full border flex bg-slate-200'>
-        <div className='border flex-1 flex-col flex items-start justify-end px-10 pb-14'>
+    <div className='h-[680px] w-full border flex bg-gradient-to-b from-purple-200 to-blue-200'>
+        <div className='border flex-1 flex-col flex items-start justify-end pl-20 pb-14'>
             <div className='block font-[700] text-[64px] leading-none capitalize mb-10'>
                 <h1>break the bad</h1>
                 <h1>keep the good</h1>
@@ -17,8 +17,8 @@ const HeroBanner = () => {
             </div>
             <Button variant="heroBanner">get started</Button>
         </div>
-        <div className='flex-1'>
-            <Lottie animationData={meditationAnimation} />
+        <div className='flex-1 flex items-center justify-center'>
+            <Lottie animationData={meditationAnimation} className='h-[90%]' />
         </div>
     </div>
   )
