3da49c737d0a207f9cf8f5f0e0e82e33965c3138
Plode <84858705+plode-v@users.noreply.github.com>
2023-10-06 15:30:08 +0700

    add animation to hero banner

--- src/components/landing-page/HeroBanner.tsx
+++ src/components/landing-page/HeroBanner.tsx
@@ -1,21 +1,24 @@
+'use client'
 import React from 'react'
 import { Button } from '../ui/button'
+import Lottie from 'lottie-react'
+import meditationAnimation from "../../public/meditationAnimation.json"
 
 const HeroBanner = () => {
   return (
-    <div className='xl:h-[calc(50vh+60px)] h-[calc(70vh+60px)] w-full border flex'>
+    <div className='xl:h-[calc(50vh+60px)] h-[calc(70vh+60px)] w-full border flex bg-slate-200'>
         <div className='border flex-1 flex-col flex items-start justify-end px-10 pb-14'>
             <div className='block font-[700] text-[64px] leading-none capitalize mb-10'>
                 <h1>break the bad</h1>
                 <h1>keep the good</h1>
             </div>
             <div className='pb-10'>
-                <p className='text-[16px]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                <p className='text-[16px]'>Your journey to a better self starts by breaking the chains of bad habits. Let Break It be the compass guiding you to a path of positive transformation.</p>
             </div>
             <Button variant="heroBanner">get started</Button>
         </div>
         <div className='flex-1'>
-            *animation here*
+            <Lottie animationData={meditationAnimation} />
         </div>
     </div>
   )
